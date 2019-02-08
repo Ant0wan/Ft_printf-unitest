6f765c66e5823743a99cb74720a57ea330b62d39
Ant0wan <abarthel@student.42.fr>
2019-02-08 18:27:13 +0100

    script to add test

--- test_to_be_added/set_tests.sh
+++ test_to_be_added/set_tests.sh
@@ -13,7 +13,18 @@
 
 
 declare printf_tests_repo="../printf_tests/"
+if [ ! -d $printf_tests_repo ]
+then
+	mkdir "$printf_tests_repo"
+fi
 declare -a FOLDER=("conversions" "width" "flags")
+for dir in "${FOLDER[@]}"
+do
+	if [ ! -d $printf_tests_repo$dir ]
+	then
+		mkdir "$printf_tests_repo$dir"
+	fi
+done	
 declare -a TYPE=("int" "long" "unisgnedint")
 
 
@@ -70,15 +81,19 @@ done
 #For putting tests:
 #sed -i.bak "s/XXX/\"Hello dude%d\", 42/g" KK_TYPE_NAME.C && rm *.bak
 
+### Format: FOLDER;TYPE;NAME;ARG ###
+
 FOLDERS_TEST=()
 IFS=$'\n'       # make newlines the only separator
 set -f          # disable globbing
 i=0
 for q in $(cat < "tests.txt"); do
-#	FOLDERS_TEST[$i]+="$(echo $($q | cut -d \t -f1))"
-	FOLDERS_TEST[$i]="$q"
-	echo $i
-	echo "${FOLDERS_TEST[$i]}"
+	FOLDERS_TEST[$i]+="$(echo $q | cut -d ';' -f1)"
+	if [ !$(cd $printf_tests_repo/${FOLDERS_TEST[$i]} 2>/dev/null) ]
+	then
+		echo "$printf_tests_repo${FOLDERS_TEST[$i]}"
+	fi
+#	echo "${FOLDERS_TEST[$i]}"
 	((i++))
 done
 
